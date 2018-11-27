39ce8deaf0967c80df2bead6fe0c9fd7b11cd795
Kieran Brahney <kieran@supportpal.com>
2018-11-27 13:11:07 +0000

    Fixed syntax error

--- lang/es/core.php
+++ lang/es/core.php
@@ -86,7 +86,7 @@ return array(
     "include_operator_name"     => "Incluir nombre del operador",
     "include_operator_name_desc" => "Include the operators name in the email 'From' field for ticket reply emails. For example: From: Joe Bloggs (YourCompany Support)",
     "include_dept_name"         => "Incluir nombre del departamento",
-    "include_dept_name_desc"    => "Incluir el nombre del departamento en el campo "De" de cualquier correo relacionado con el ticket. Tenga en cuenta que esto normalmente creará un nuevo hilo de correos si un determinado ticket cambia de departamento." ,
+    "include_dept_name_desc"    => "Incluir el nombre del departamento en el campo 'De' de cualquier correo relacionado con el ticket. Tenga en cuenta que esto normalmente creará un nuevo hilo de correos si un determinado ticket cambia de departamento." ,
     "global_email_header"       => "Global Email Header",
     "global_email_header_desc"  => "Add a header to all emails sent out by the system. HTML is accepted.",
     "global_email_footer"       => "Global Email Footer",
@@ -333,7 +333,7 @@ return array(
     "and_number_others"         => "and :number other|and :number others",
     "user_templates"            => "Plantillas del usuario",
     "operator_templates"        => "Plantillas del operador",
-    
+
     /*
      * 2.2.0
      */
