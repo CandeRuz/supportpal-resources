b2e55cf16e41256b2566443b469912b5ff0af5a2
CandeRuz <42004377+CandeRuz@users.noreply.github.com>
2019-03-18 12:21:20 +0100

    Update license.php

--- lang/es/license.php
+++ lang/es/license.php
@@ -2,31 +2,31 @@
 
 return array(
 
-    "license"           => "License",
-    "license_key"       => "License Key",
-    "error"             => "Error!",
-    "submit"            => "Submit",
-    "update"            => "Update",
+    "license"           => "Licencia",
+    "license_key"       => "Clave de licencia",
+    "error"             => "¡Error!",
+    "submit"            => "Enviar",
+    "update"            => "Actualizar",
 
-    "reasons"           => "Possible Reasons",
-    "new_key"           => "New License Key",
-    "enter_key"         => "If you have a new license key, please enter it below and press Submit to continue.",
-    "assistance"        => "Still Need Assistance?",
+    "reasons"           => "Posibles razones",
+    "new_key"           => "Nueva clave de licencia",
+    "enter_key"         => "Si tienes una nueva clave de licencia, por favor, introdúcela más abajo y haz click en Enviar para continuar.",
+    "assistance"        => "¿Todavía necesitas asistencia?",
 
-    "no_license"        => "No license found.",
-    "license_invalid"   => "License Invalid",
+    "no_license"        => "No se ha encontrado ninguna licencia.",
+    "license_invalid"   => "Licencia inválida",
     "invalid"           => "Invalid",
 
     "incorrect_license" => "An incorrect license key has been used.",
-    "ip_changed"        => "The IP address your system is using has changed.",
-    "domain_changed"    => "The domain you are using has changed.",
+    "ip_changed"        => "La dirección IP que tu sistema utiliza ha cambiado.",
+    "domain_changed"    => "El dominio que utilizas ha cambiado.",
     "reason_1_desc"     => "If your license key is correct please try re-issuing it from the client area before contacting support.",
 
     /*
      * 2.1.0
      */
-    "server_response"   => "Server Response",
-    "need_to_reissue"   => "You may need to <strong>reissue</strong> the license at our <a href='http://www.supportpal.com/manage/' target='_blank'>client area</a>.",
+    "server_response"   => "Respuesta del servidor",
+    "need_to_reissue"   => "Puede que necesites <strong>reissue</strong> la licencia en nuestro <a href='http://www.supportpal.com/manage/' target='_blank'>client area</a>.",
 
     /*
      * 2.2.0
@@ -36,7 +36,7 @@ return array(
     /*
      * 2.3.1
      */
-    "license_valid"     => "License Valid!",
+    "license_valid"     => "¡Licencia válida!",
 
     /*
      * 2.4.1
