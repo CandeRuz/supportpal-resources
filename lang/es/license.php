ed13381a4311ba2a6c53df5663f56c5e442d6000
CandeRuz <42004377+CandeRuz@users.noreply.github.com>
2019-09-18 15:01:30 +0100

    Update license.php

--- lang/es/license.php
+++ lang/es/license.php
@@ -26,7 +26,7 @@ return array(
      * 2.1.0
      */
     "server_response"   => "Respuesta del servidor",
-    "need_to_reissue"   => "Puede que necesites <strong>reissue</strong> la licencia en nuestro <a href='http://www.supportpal.com/manage/' target='_blank'>client area</a>.",
+    "need_to_reissue"   => "Puede que necesites <strong>reemitir</strong> la licencia en nuestro <a href='http://www.supportpal.com/manage/' target='_blank'>área del cliente</a>.",
 
     /*
      * 2.2.0
@@ -41,6 +41,6 @@ return array(
     /*
      * 2.4.1
      */
-    "multi_ip_support"  => "Considera activar <a href=':href' target='_blank'>Multi-IP support</a> en el área del cliente para definir direcciones IP adicionales.",
+    "multi_ip_support"  => "Considera activar <a href=':href' target='_blank'>la asistencia multi-IP</a> en el área del cliente para definir direcciones IP adicionales.",
 
 );
